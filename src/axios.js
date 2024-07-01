9eb752f4af7f954ba1e2135865510380adeeabf7
Zhavokh1r <zhavo.kh1r3101@gmail.com>
2024-07-01 19:59:45 +0500

    dz tayyor

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://crudcrud.com/api/57a1dd4d1d2e4a27801a087ea943b028"
+  baseURL: "https://crudcrud.com/api/56c604a1ac8d4a908f7bc0ca9a9d5fe4"
 });
